5cd295e1cacd69872a5a3e644ba81a7e9f4f646a
Pey <sophiaraudhah@gmail.com>
2024-06-03 11:44:32 +0700

    remove tags dan stopwords

--- src/NewsCategorization.java
+++ src/NewsCategorization.java
@@ -7,30 +7,56 @@ import com.opencsv.CSVReader;
 public class NewsCategorization {
     public static void main(String[] args) throws Exception {
         ArrayList<String[]> data = new ArrayList<>(); // ArrayList to store the data
+
+        DataPreprocessor preprocessor = new DataPreprocessor();
         
         try (CSVReader csvReader = new CSVReader(new FileReader("resources/BBC News Train.csv"))) {
             String[] lines;
+            boolean isFirstRow = true;
+            
             while ((lines = csvReader.readNext()) != null) {
-                data.add(lines); // Add each row to the ArrayList
+                // skip header row
+                if (isFirstRow) {
+                    isFirstRow = false;
+                    continue;
+                }
+
+                // Process each field in the row
+                if (lines.length > 1) {
+                    lines[1] = preprocessor.removeTags(lines[1]);
+                    lines[1] = preprocessor.removeStopwords(lines[1]);
+                }
+                data.add(lines); // Add each processed row to the ArrayList
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         // Print the first three records from the ArrayList
         int count = 0;
         for (String[] row : data) {
             if (count < 3) {
-                for (String value : row) {
-                    System.out.print(value + "**");
-                }
+                String articleId = row[0];
+                String originalText = row[1];
+                String category = row[2];
+
+                String textWithoutTags = DataPreprocessor.removeTags(originalText);
+                String cleanText = DataPreprocessor.removeStopwords(textWithoutTags);
+
+                System.out.println("ArticleId: " + articleId);
+                System.out.println("Category: " + category);
+                System.out.println("Original Text: " + originalText);
+                System.out.println("Text without HTML tags: " + textWithoutTags);
+                System.out.println("Cleaned Text: " + cleanText);
                 System.out.println();
+
                 count++;
             } else {
                 break; // Stop iterating after printing three records
             }
+
         }
     }
 }
